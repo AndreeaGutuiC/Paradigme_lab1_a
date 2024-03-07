833fdfb2d5918e93f749a4ddcbc3c4f3fd7f0343
AndreeaGutuiC <andreeacarolina.gutui@ulbsibiu.ro>
2024-03-07 14:05:40 +0200

    Punctul a)

---
+++ src/LitereAlfabet.java
@@ -0,0 +1,14 @@
+public class LitereAlfabet {
+
+    public void Afis() {
+        int lungime = 'z' - 'a';
+        for (int i = 0; i < lungime; i++) {
+            char alfa = (char) ('a' + i);
+            System.out.print(alfa);
+        }
+    }
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        LitereAlfabet l = new LitereAlfabet();
+        l.Afis();
+    }
+}
