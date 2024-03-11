d42c7e6186c640cd678d78e816ace31a8f7ca6ae
AndreeaGutuiC <andreeacarolina.gutui@ulbsibiu.ro>
2024-03-11 22:11:03 +0200

    Punctul c)

--- src/Main.java
+++ src/Main.java
@@ -18,10 +18,21 @@ public class Main {
         for (int i = 0; i < k; i++) {
             System.out.println(sir[i]);
         }
+
+        int[] vector = new int[]{12, 3, 500, 1200, 50, 40, 100, 250, 8};
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++)
+                System.out.print(vector[3*i + j] + " ");
+            System.out.println();
+        }
+
+
     }
     public static boolean isVowel(char in){
         char up_in = Character.toUpperCase(in);
         if(up_in=='A' || up_in == 'E' || up_in == 'I' || up_in == 'O' || up_in == 'U') return true;
-        return false;}
+        return false;
+
+    }
 
 }
